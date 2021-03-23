f557d49aafed0f2a834abef97d4fbcb3998a3091
seongsik-choi <rlawlsdk1225@gmail.com>
2021-03-23 17:12:25 +0900

    .jsp

---
+++ 0323_2Spring MVC Annotation(src)/java/dev/mvc/calc/Test2.java
@@ -0,0 +1,12 @@
+/*
+0323
+2) @Controller 에노테이션이 생략되어 있어 객체 생성이 안됨
+▷ /src/main/java/dev.mvc.calc.Test2.java 
+ */
+package dev.mvc.calc;
+public class Test2 {
+  public Test2() {
+    System.out.println("--> test2 객체 생성됨.");
+  }
+ 
+}
---
+++ 0323_2Spring MVC Annotation(src)/java/dev/mvc/calc2/Test3.java
@@ -0,0 +1,20 @@
+/*
+0323
+3) @Controller 에노테이션을 선언했지만 패키지가 달라 객체 생성이 안됨.
+▷ /src/main/java/dev.mvc.calc2.Test3.java
+
+ -> 해결 방법
+ ▶ servlet-contex.xml에 선언하여  @Controller Annotation이 선언된 클래스를 자동 생성
+<context:component-scan base-package="dev.mvc.cal2" />
+ */
+package dev.mvc.calc2;
+import org.springframework.stereotype.Controller;
+ 
+@Controller
+public class Test3 {
+ 
+  public Test3() {
+    System.out.println("--> test3 객체 생성됨.");
+  }
+ 
+}
